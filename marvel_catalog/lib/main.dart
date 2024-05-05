c921e6f3f7293442f4d67304d06924b7816e9b4c
Jeferson A O <oliveira.jefersonaraujo@gmail.com>
2024-05-05 20:47:11 -0300

    BUGFIX: scrolView infinite

--- marvel_catalog/lib/main.dart
+++ marvel_catalog/lib/main.dart
@@ -3,10 +3,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:marvel_catalog/helpers/firebase_options.dart';
 import 'package:marvel_catalog/stores/character_store.dart';
 import 'package:marvel_catalog/views/character_detail.dart';
+import 'package:marvel_catalog/views/character_list.dart';
 import 'package:marvel_catalog/views/pass_reset.dart';
 import 'views/login_page.dart';
 import 'views/register_page.dart';
-import 'views/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,15 +27,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Heróis',
         theme: kbthem,
-        initialRoute: '/characterlist',
+        initialRoute: '/home',
         routes: {
           '/': (context) => LoginPage(),
           '/register': (context) => RegisterPage(),
-          '/home': (context) => Characterlist(),
+          '/home': (context) => CharactersList(),
           '/passreset': (context) => PasswordResetPage(),
-          '/characterlist': (context) => Characterlist(),
           '/characterdetail': (context) =>
-              CharacterDetailsPage(characterId: 1011334)
+              CharacterDetailsPage(characterId: 1011334) //id padrão do 3d man
         });
   }
 }
--- marvel_catalog/lib/views/character_list.dart
+++ marvel_catalog/lib/views/character_list.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:marvel_catalog/shared/components/mv_drawer.dart';
 import 'package:marvel_catalog/stores/character_store.dart';
 
 class CharactersList extends StatefulWidget {
-  final CharacterStore _characterStore = CharacterStore();
-
-  //CharactersList({super.key, required this.CharactersStore});
-
   @override
   _CharactersListState createState() => _CharactersListState();
+
+  final CharacterStore _characterStore = CharacterStore();
 }
 
 class _CharactersListState extends State<CharactersList> {
@@ -37,32 +37,43 @@ class _CharactersListState extends State<CharactersList> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ListView.builder(
-        controller: _scrollController,
-        itemCount: widget._characterStore.characters.length + 1,
-        itemBuilder: (context, index) {
-          if (index < widget._characterStore.characters.length) {
-            final character = widget._characterStore.characters[index];
-            return ListTile(
-              leading: Image.network(character.imageUrl),
-              title: Text(character.name),
-            );
-          } else if (!widget._characterStore.isLoading) {
-            return Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  widget._characterStore.fetchCharacters();
-                },
-                child: const Text('Load More'),
-              ),
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Personagens'),
+      ),
+      drawer: MvDrawer(),
+      body: Observer(
+        builder: (_) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+                controller: _scrollController,
+                itemCount: widget._characterStore.characters.length + 1,
+                itemBuilder: (context, index) {
+                  if (index < widget._characterStore.characters.length) {
+                    final character = widget._characterStore.characters[index];
+                    return Card(
+                      child: ListTile(
+                        onTap: () => Navigator.pushNamed(
+                            context, '/Characterdetail',
+                            arguments: {character.id}),
+                        title: Text(character.name),
+                        leading: Image.network(character.imageUrl),
+                        subtitle: Text(character.description.isNotEmpty &&
+                                character.description.length > 50
+                            ? '${character.description.substring(0, 50)}...'
+                            : character.description),
+                      ),
+                    );
+                  } else if (!widget._characterStore.isLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                }),
+          );
         },
       ),
     );
